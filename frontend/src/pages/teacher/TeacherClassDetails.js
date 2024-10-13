948d6e0cfad5e3b1453fc1188b655dd548bb0d3a
n-keerthi-gayathri <keerthigayathrinissankararao40@gmail.com>
2024-10-13 19:34:58 +0530

    Professor UI Update

--- frontend/src/pages/teacher/TeacherClassDetails.js
+++ frontend/src/pages/teacher/TeacherClassDetails.js
@@ -1,178 +1,253 @@
 import { useEffect } from "react";
-import * as React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { getClassStudents } from "../../redux/sclassRelated/sclassHandle";
-import { Paper, Box, Typography, ButtonGroup, Button, Popper, Grow, ClickAwayListener, MenuList, MenuItem } from '@mui/material';
-import { BlackButton, BlueButton} from "../../components/buttonStyles";
+import {
+  Paper,
+  Box,
+  Typography,
+  ButtonGroup,
+  Button,
+  Popper,
+  Grid,
+  Grow,
+  ClickAwayListener,
+  MenuList,
+  MenuItem,
+} from "@mui/material";
+import { BlackButton, BlueButton } from "../../components/buttonStyles";
 import TableTemplate from "../../components/TableTemplate";
 import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 
 const TeacherClassDetails = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
-    const { sclassStudents, loading, error, getresponse } = useSelector((state) => state.sclass);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { sclassStudents, loading, error, getresponse } = useSelector(
+    (state) => state.sclass
+  );
 
-    const { currentUser } = useSelector((state) => state.user);
-    const classID = currentUser.teachSclass?._id
-    const subjectID = currentUser.teachSubject?._id
+  const { currentUser } = useSelector((state) => state.user);
+  const classID = currentUser.teachSclass?._id;
+  const subjectID = currentUser.teachSubject?._id;
 
-    useEffect(() => {
-        dispatch(getClassStudents(classID));
-    }, [dispatch, classID])
+  useEffect(() => {
+    dispatch(getClassStudents(classID));
+  }, [dispatch, classID]);
 
-    if (error) {
-        console.log(error)
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    const studentColumns = [
-        { id: 'name', label: 'Name', minWidth: 170 },
-        { id: 'rollNum', label: 'Roll Number', minWidth: 100 },
-    ]
+  const studentColumns = [
+    { id: "name", label: "Name", minWidth: 170 },
+    { id: "rollNum", label: "Roll Number", minWidth: 100 },
+  ];
 
-    const studentRows = sclassStudents.map((student) => {
-        return {
-            name: student.name,
-            rollNum: student.rollNum,
-            id: student._id,
-        };
-    })
+  const studentRows = sclassStudents.map((student) => {
+    return {
+      name: student.name,
+      rollNum: student.rollNum,
+      id: student._id,
+    };
+  });
 
-    const StudentsButtonHaver = ({ row }) => {
-        const options = ['Take Attendance', 'Provide Marks'];
+  const StudentsButtonHaver = ({ row }) => {
+    const options = ["Take Attendance", "Provide Marks"];
 
-        const [open, setOpen] = React.useState(false);
-        const anchorRef = React.useRef(null);
-        const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-        const handleClick = () => {
-            console.info(`You clicked ${options[selectedIndex]}`);
-            if (selectedIndex === 0) {
-                handleAttendance();
-            } else if (selectedIndex === 1) {
-                handleMarks();
-            }
-        };
-
-        const handleAttendance = () => {
-            navigate(`/Teacher/class/student/attendance/${row.id}/${subjectID}`)
-        }
-        const handleMarks = () => {
-            navigate(`/Teacher/class/student/marks/${row.id}/${subjectID}`)
-        };
-
-        const handleMenuItemClick = (event, index) => {
-            setSelectedIndex(index);
-            setOpen(false);
-        };
-
-        const handleToggle = () => {
-            setOpen((prevOpen) => !prevOpen);
-        };
-
-        const handleClose = (event) => {
-            if (anchorRef.current && anchorRef.current.contains(event.target)) {
-                return;
-            }
-
-            setOpen(false);
-        };
-        return (
-            <>
-                <BlueButton
-                    variant="contained"
-                    onClick={() =>
-                        navigate("/Teacher/class/student/" + row.id)
-                    }
-                >
-                    View
-                </BlueButton>
-                <React.Fragment>
-                    <ButtonGroup variant="contained" ref={anchorRef} aria-label="split button">
-                        <Button onClick={handleClick}>{options[selectedIndex]}</Button>
-                        <BlackButton
-                            size="small"
-                            aria-controls={open ? 'split-button-menu' : undefined}
-                            aria-expanded={open ? 'true' : undefined}
-                            aria-label="select merge strategy"
-                            aria-haspopup="menu"
-                            onClick={handleToggle}
-                        >
-                            {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-                        </BlackButton>
-                    </ButtonGroup>
-                    <Popper
-                        sx={{
-                            zIndex: 1,
-                        }}
-                        open={open}
-                        anchorEl={anchorRef.current}
-                        role={undefined}
-                        transition
-                        disablePortal
-                    >
-                        {({ TransitionProps, placement }) => (
-                            <Grow
-                                {...TransitionProps}
-                                style={{
-                                    transformOrigin:
-                                        placement === 'bottom' ? 'center top' : 'center bottom',
-                                }}
-                            >
-                                <Paper>
-                                    <ClickAwayListener onClickAway={handleClose}>
-                                        <MenuList id="split-button-menu" autoFocusItem>
-                                            {options.map((option, index) => (
-                                                <MenuItem
-                                                    key={option}
-                                                    disabled={index === 2}
-                                                    selected={index === selectedIndex}
-                                                    onClick={(event) => handleMenuItemClick(event, index)}
-                                                >
-                                                    {option}
-                                                </MenuItem>
-                                            ))}
-                                        </MenuList>
-                                    </ClickAwayListener>
-                                </Paper>
-                            </Grow>
-                        )}
-                    </Popper>
-                </React.Fragment>
-            </>
-        );
+    const handleClick = () => {
+      console.info(`You clicked ${options[selectedIndex]}`);
+      if (selectedIndex === 0) {
+        handleAttendance();
+      } else if (selectedIndex === 1) {
+        handleMarks();
+      }
     };
 
-    return (
-        <>
-            {loading ? (
-                <div>Loading...</div>
-            ) : (
-                <>
-                    <Typography variant="h4" align="center" gutterBottom>
-                        Class Details
-                    </Typography>
-                    {getresponse ? (
-                        <>
-                            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' }}>
-                                No Students Found
-                            </Box>
-                        </>
-                    ) : (
-                        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                            <Typography variant="h5" gutterBottom>
-                                Students List:
-                            </Typography>
+    const handleAttendance = () => {
+      navigate(`/Teacher/class/student/attendance/${row.id}/${subjectID}`);
+    };
+    const handleMarks = () => {
+      navigate(`/Teacher/class/student/marks/${row.id}/${subjectID}`);
+    };
 
-                            {Array.isArray(sclassStudents) && sclassStudents.length > 0 &&
-                                <TableTemplate buttonHaver={StudentsButtonHaver} columns={studentColumns} rows={studentRows} />
-                            }
-                        </Paper>
-                    )}
-                </>
+    const handleMenuItemClick = (event, index) => {
+      setSelectedIndex(index);
+      setOpen(false);
+    };
+
+    const handleToggle = () => {
+      setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+      if (anchorRef.current && anchorRef.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    };
+    return (
+      <>
+        <BlueButton
+          variant="contained"
+          onClick={() => navigate("/Teacher/class/student/" + row.id)}
+          style={{ margin: "5px" }}
+        >
+          View
+        </BlueButton>
+        <React.Fragment>
+          <ButtonGroup
+            variant="contained"
+            ref={anchorRef}
+            aria-label="split button"
+          >
+            <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+            <BlackButton
+              size="small"
+              aria-controls={open ? "split-button-menu" : undefined}
+              aria-expanded={open ? "true" : undefined}
+              aria-label="select merge strategy"
+              aria-haspopup="menu"
+              onClick={handleToggle}
+            >
+              {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+            </BlackButton>
+          </ButtonGroup>
+          <Popper
+            sx={{
+              zIndex: 1,
+            }}
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === "bottom" ? "center top" : "center bottom",
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="split-button-menu" autoFocusItem>
+                      {options.map((option, index) => (
+                        <MenuItem
+                          key={option}
+                          disabled={index === 2}
+                          selected={index === selectedIndex}
+                          onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
             )}
-        </>
+          </Popper>
+        </React.Fragment>
+      </>
     );
+  };
+
+  return (
+    <>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            textAlign: "center",
+            fontSize: "calc(10px + 1.5vw)",
+          }}
+        >
+          Loading...
+        </div>
+      ) : (
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "center",
+              padding: "10px",
+            }}
+          >
+            <Typography
+              variant="h4"
+              align="center"
+              gutterBottom
+              style={{ fontSize: "calc(24px + 1vw)" }}
+            >
+              Class Details
+            </Typography>
+          </div>
+          {getresponse ? (
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                No Students Found
+              </Box>
+            </>
+          ) : (
+            <div
+              style={{
+                padding: "16px",
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+              }}
+            >
+              <Typography
+                variant="h5"
+                gutterBottom
+                style={{
+                  textAlign: "left",
+                  fontSize: "calc(15px + 1vw)",
+                  marginBottom: "16px",
+                }}
+              >
+                Students List:
+              </Typography>
+              <Paper
+                sx={{
+                  width: "100%",
+                  overflow: "hidden",
+                  padding: "16px",
+                  boxShadow: 3,
+                }}
+              >
+                {Array.isArray(sclassStudents) && sclassStudents.length > 0 && (
+                  <TableTemplate
+                    buttonHaver={StudentsButtonHaver}
+                    columns={studentColumns}
+                    rows={studentRows}
+                  />
+                )}
+              </Paper>
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default TeacherClassDetails;
